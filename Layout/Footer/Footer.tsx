76f8ca8eb12bb98ea8692fa5e22c327cf93ec370
Nurbolot Berdibekov <nurchik0225@gmail.com>
2023-03-05 18:07:52 +0600

    Add server side rendering and useContext Menu

--- Layout/Footer/Footer.tsx
+++ Layout/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import cn from 'classnames';
 import { format } from 'date-fns';
 
 
-export const Footer= ({ className, ...props }: FooterProps): JSX.Element => {
+export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
     return (
         <footer className={cn(className, styles.footer)} {...props}>
             <div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
+import { GetStaticProps } from 'next';
 import React, { useState } from "react";
 import { Button, Htag, P, Rating, Tag } from "../components";
 import { withLayout } from "../Layout/Layout";
+import axios from 'axios';
+import { MenuItem } from '../interfaces/menu.interface';
 
-function Home(): JSX.Element {
+function Home({ menu }): JSX.Element {
 	const [rating, setRating] = useState<number>(4);
 
 	return (
@@ -23,3 +26,21 @@ function Home(): JSX.Element {
 }
 
 export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+	const firstCategory = 0;
+	const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
+		firstCategory
+	});
+	return {
+		props: {
+			menu,
+			firstCategory
+		}
+	};
+};
+
+interface HomeProps extends Record<string, unknown> {
+	menu: MenuItem[];
+	firstCategory: number;
+}
