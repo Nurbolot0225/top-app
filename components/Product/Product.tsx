6381a5a0d76672482ecb45303f384a194420acd9
Nurbolot Berdibekov <nurchik0225@gmail.com>
2023-03-08 15:49:08 +0600

    lazy next image

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -5,37 +5,57 @@ import { Card } from '../Card/Card';
 import { Rating } from '../Rating/Rating';
 import { Tag } from '../Tag/Tag';
 import { Button } from '../Button/Button';
+import { declOfNum, priceRu } from '../../helpers/helpers';
+import { Divider } from '../Divider/Divider';
+import Image from 'next/image';
 
 
 export const Product = ({ product, className, ...props}: ProductProps): JSX.Element => {
     return (
         <Card className={styles.product}>
-            <div className={styles.logo}><img src={product.image} alt={product.title} /></div>
+            <div className={styles.logo}>
+                <Image 
+                    src={product.image} 
+                    alt={product.title}
+                    width={70}
+                    height={70}
+                />
+            </div>
             <div className={styles.title}>{product.title}</div>
-            <div className={styles.price}>{product.price}</div>
-            <div className={styles.credit}>{product.credit}</div>
+            <div className={styles.price}>{priceRu(product.price)}{product.oldPrice && <Tag className={styles.oldPrice} color='green'>{priceRu(product.price - product.oldPrice)}</Tag>}</div>
+            <div className={styles.credit}>{priceRu(product.credit)}/<span className={styles.month}>мес</span></div>
             <div className={styles.rating}><Rating rating={product.reviewAvg ?? product.initialRating} /></div>
-            <div className={styles.tags}>{product.categories.map(c => <Tag key={c} color='ghost'>{c}</Tag>)}</div>
+            <div className={styles.tags}>{product.categories.map(c => <Tag key={c} className={styles.category} color='ghost'>{c}</Tag>)}</div>
             <div className={styles.priceTitle}>цена</div>
             <div className={styles.creditTitle}>кредит</div>
-            <div className={styles.rateTitle}>{product.reviewCount} отзывов</div>
+            <div className={styles.rateTitle}>{product.reviewCount} {declOfNum(product.reviewCount, ['отзыв', 'отзыва', 'отзывов'])}</div>
+            <Divider className={styles.hr} />
             <div className={styles.hr}><hr /></div>
             <div className={styles.description}>{product.description}</div>
-            <div className={styles.feature}>фичи</div>
+            <div className={styles.feature}>
+                {product.characteristics.map(c => {
+                    <div className={styles.characteristics} key={c.name}>
+                        <span className={styles.characteristicsName}>{c.name}</span>
+                        <span className={styles.characteristicsDots}></span>
+                        <span className={styles.characteristicsValue}>{c.value}</span>
+                    </div>;
+                })}
+            </div>
             <div className={styles.advBlock}>
                 <div className={styles.advantages}>
                     <div>Приемущества</div>
                     <div>{product.advantages}</div>
                 </div>
-                <div className={styles.disadvantages}>
-                    <div>Недостатки</div>
+                {product.disadvantages && <div className={styles.disadvantages}>
+                    <div className={styles.advTitle}>Недостатки</div>
                     <div>{product.disadvantages}</div>
-                </div>
+                </div>}
             </div>
+            <Divider className={styles.hr} />
             <div className={styles.hr}><hr /></div>
             <div className={styles.actions}>
                 <Button apperance='primary'>Узнать подробнее</Button>
-                <Button apperance='ghost' arrow='right'>Читать отзывы</Button>
+                <Button apperance='ghost' arrow='right' className={styles.reviewButton}>Читать отзывы</Button>
             </div>
         </Card>
     );
--- helpers/helpers.tsx
+++ helpers/helpers.tsx
@@ -5,7 +5,6 @@ import ProductsIcon from './icons/products.svg';
 import { TopLevelCategory } from '../interfaces/page.interface';
 import { FirstLevelMenuItem } from '../interfaces/menu.interface';
 
-
 export const firstLevelMenu: FirstLevelMenuItem[] = [
 	{ route: 'courses', name: 'Курсы', icon: <CoursesIcon />, id: TopLevelCategory.Courses },
 	{ route: 'services', name: 'Сервисы', icon: <ServicesIcon />, id: TopLevelCategory.Services },
@@ -14,3 +13,8 @@ export const firstLevelMenu: FirstLevelMenuItem[] = [
 ];
 
 export const priceRu = (price: number): string => price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ').concat(' ₽');
+
+export const declOfNum = (number: number, titles: [string, string, string]): string => {
+	const cases = [2, 0, 1, 1, 1, 2]
+	return titles[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5]];
+};
